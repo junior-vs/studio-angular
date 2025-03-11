0f7fe63e1cf3b9f93fb081e73fc9afb1c25955ec
VALDIR JUNIOR <valdir.junior.dev@gmail.com>
2025-03-11 10:41:23 -0300

    aula 2 Add home page layout with destinations and testimonials sections

--- jornada-milhas/src/app/pages/home/home.component.ts
+++ jornada-milhas/src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../shared/banner/banner.component";
+import { ContainerComponent } from "../../shared/container/container.component";
+import { CardBuscaComponent } from "../../shared/card-busca/card-busca.component";
+import { CardDepoimentosComponent } from "../../shared/card-depoimentos/card-depoimentos.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent],
+  imports: [BannerComponent, ContainerComponent, CardBuscaComponent, CardDepoimentosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
